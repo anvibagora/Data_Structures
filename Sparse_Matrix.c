6330b983fc9c1aaaa7bae246464b5e50136b6960
Anvi Bagora <anvibagora@gmail.com>
2026-07-10 12:37:17 +0000

    save

--- Sparse_Matrix.c
+++ Sparse_Matrix.c
@@ -1,10 +1,10 @@
-//GOTTA WORK ON THIS 
-
+//TRANSPOSE NOT WORKING LIKE IT SHOULD BE
 #include <stdio.h>
+#include <stdlib.h>
 
 void accept(int a[10][10],int m,int n){
 	int i,j;
-	printf("\nEnter values of matrix: ");
+	printf("\nEnter values of sparse matrix: ");
 	for(i=0; i<m; i++){
 		for(j=0; j<n; j++){
 			scanf("%d",&a[i][j]);
@@ -14,6 +14,7 @@ void accept(int a[10][10],int m,int n){
 
 void display(int a[10][10], int m,int n){
 	int i,j;
+	printf("\n");
 	for(i=0; i<m; i++){
 		for(j=0; j<n; j++){
 			printf("\t%d",a[i][j]);
@@ -23,13 +24,13 @@ void display(int a[10][10], int m,int n){
 }
 
 int main(){
-	int a[10][10], b[10][10], m, n,i,j,k;
-	
-	printf("\nEnter number of rows and columns for matrix: ");
+	int a[10][10],b[10][10],c[10][10],m,n,i,j,k;
+	printf("\nEnter number of rows and columns for sparse matrix: ");
 	scanf("%d%d", &m, &n);
 	accept(a,m,n);
 	display(a,m,n);
-
+	
+	//Sparse matrix to compact form
     b[0][0]=m;
     b[0][1]=n;
     k=0;
@@ -44,6 +45,31 @@ int main(){
         }
     }
     b[0][2]=k;
+	printf("\nCompact form of the sparse matric is");
+    display(b,k+1,3);
 
-    display(b,m+1,3);
+	//Transpose of compact form
+	c[0][0]=b[0][1];
+	c[0][1]=b[0][0];
+	c[0][2]=b[0][2];
+	int t=b[0][2];
+	if(t<=0){
+		printf("ERROR! transpose cannot be performed!");
+		exit(0);
+	}else{
+		printf("Transpose of compact form of sparse matrix is: ");
+		int q=1;
+		for(int col=0;col<=n;col++){
+			for(int p=1;p<=t;p++){
+				if(a[p][1]==col){
+					c[q][0]=b[q][1];
+					c[q][1]=b[q][0];
+					c[q][2]=b[q][2];
+					q+=1;
+				}
+			}
+		}
+	}
+	display(c,k+1,3);
+	return 0;
 }
