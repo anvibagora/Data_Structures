84006b42d3c83fe6f5fbb5a43839bf3ac76d7ca6
Anvi Bagora <anvibagora@gmail.com>
2026-07-13 13:48:05 +0000

    save

--- Matrix_Operations.c
+++ Matrix_Operations.c
@@ -21,36 +21,43 @@ void display(int a[10][10], int m,int n){
 	}
 }
 
-void addition(int a[10][10], int b[10][10], int m, int n){
+void addition(int a[10][10], int b[10][10], int m, int n, int x, int y){
 	int c[10][10],i ,j;
-	for(i=0; i<m; i++){
-		for(j=0; j<n; j++){
-			c[i][j] = a[i][j] + b[i][j];
+	if(x==m && y==n){
+		for(i=0; i<m; i++){
+			for(j=0; j<n; j++){
+				c[i][j] = a[i][j] + b[i][j];
+			}
 		}
 	}
 	display(c,m,n);
 }
 	
-void subtraction(int a[10][10], int b[10][10], int m, int n){
+void subtraction(int a[10][10], int b[10][10], int m, int n, int x, int y){
 	int c[10][10],i ,j;
-	for(i=0; i<m; i++){
-		for(j=0; j<n; j++){
-			c[i][j] = a[i][j] - b[i][j];
+	if(x==m && y==n){
+		for(i=0; i<m; i++){
+			for(j=0; j<n; j++){
+				c[i][j] = a[i][j] - b[i][j];
+			}
 		}
 	}
 	display(c,m,n);
-}	
+}
 
-void multiplication(int a[10][10], int b[10][10], int m, int n){
+void multiplication(int a[10][10], int b[10][10], int m, int n, int x, int y){
 	int c[10][10]={0},i ,j,k;
-	for(i=0; i<m; i++){
-		for(j=0; j<n; j++){
-			for(k=0; k<n; k++){
-			c[i][j] += a[i][k] * b[k][j];
-			}	
+	if(n==x){
+		for(i=0; i<m; i++){
+			for(j=0; j<y; j++){
+				c[i][j]=0;
+				for(k=0; k<n; k++){
+					c[i][j] += a[i][k] * b[k][j];
+				}	
+			}
 		}
 	}
-	display(c,m,n);
+	display(c,m,y);
 }
 
 void transpose(int a[10][10], int m, int n){
@@ -64,14 +71,16 @@ void transpose(int a[10][10], int m, int n){
 }
 
 int main(){
-	int a[10][10], b[10][10], m, n, ch;
+	int a[10][10], b[10][10], m, n, ch, x, y;
 	
-	printf("\nEnter number of rows and columns for matrices: ");
+	printf("\nEnter number of rows and columns for matrices A: ");
 	scanf("%d%d", &m, &n);
 	accept(a,m,n);
 	display(a,m,n);
-	accept(b,m,n);
-	display(b,m,n);
+	printf("\nEnter number of rows and columns for matrices A: ");
+	scanf("%d%d", &x, &y);
+	accept(b,x,y);
+	display(b,x,y);
 	
 	while(1){
 		printf("\n1. Addition");
@@ -83,11 +92,11 @@ int main(){
 		scanf("%d",&ch);
 		
 		switch(ch){
-			case 1: addition(a,b,m,n);
+			case 1: addition(a,b,m,n,x,y);
 				break;
-			case 2: subtraction(a,b,m,n);
+			case 2: subtraction(a,b,m,n,x,y);
 				break;
-			case 3: multiplication(a,b,m,n);
+			case 3: multiplication(a,b,m,n,x,y);
 				break;
 			case 4: transpose(a,m,n);
 				break;
