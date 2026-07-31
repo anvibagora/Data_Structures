17d8019c2683d0f748ae75d6f55203060f125b67
Anvi Bagora <anvibagora@gmail.com>
2026-07-31 11:36:48 +0000

    save

---
+++ polynomials.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+struct poly{
+    int coef;
+    int exp;
+};
+
+void polydd(int p1[100], int p2[100], int max1, int max2){
+    int i=0,j=0,k=0;
+    while(i<max1 && j<max2){
+        if(p1[i].exp>p2[j].exp){
+
+        }    
+    }
+}
