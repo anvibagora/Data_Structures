d81d327ecd26283a20bbb48eb5408c270b83325c
Anvi Bagora <anvibagora@gmail.com>
2026-07-31 11:50:48 +0000

    save

--- polynomials.c
+++ polynomials.c
@@ -6,11 +6,60 @@ struct poly{
     int exp;
 };
 
-void polydd(int p1[100], int p2[100], int max1, int max2){
+int main(){
+    int max1,max2,i;
+    struct poly p1[100],p2[100],p3[100];
+    printf("Number of terms in 1st polynomial: ");
+    scanf("%d",&max1);
+    for(i=0;i<max1;i++){
+        printf("Coeffecient: ");
+        scanf("%d",&p1[i].coef);
+        printf("Exponential: ");
+        scanf("%d",&p1[i].exp);
+    }
+    printf("Number of terms in 2st polynomial: ");
+    scanf("%d",&max2);
+    for(i=0;i<max2;i++){
+        printf("Coeffecient: ");
+        scanf("%d",&p2[i].coef);
+        printf("Exponential: ");
+        scanf("%d",&p2[i].exp);
+    }
+    
+}
+
+void polydd(struct poly p1[100], struct poly p2[100], int max1, int max2){
     int i=0,j=0,k=0;
+    int temp;
+    struct poly p3[100];
     while(i<max1 && j<max2){
         if(p1[i].exp>p2[j].exp){
-
-        }    
+            p3[k]=p1[i];
+            k++;
+            i++;
+        }else if(p1[i].exp<p2[j].exp){
+            p3[k]=p2[i];
+            k++;
+            j++;
+        }else{
+            temp=p1[i].coef+p2[j].coef;
+            if(temp!=0){
+                p3[k].exp=p1[i].exp;
+                p3[k].coef=temp;
+                i++;
+                j++;
+                k++;
+            }
+        }
+    }
+    while(i<max1){
+        p3[k]=p1[i];
+        k++;
+        i++;
+    }
+    while(i<max1){
+        p3[k]=p2[j];
+        k++;
+        j++;
     }
 }
